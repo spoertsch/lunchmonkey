a4c1288fdbb2b7df376906f0cefd1c27efdf97ac
spoertsch <spoertsch@gmail.com>
2014-09-06 22:37:36 +0200

    This resolves #2, #3, #4 and #5

--- public/javascripts/LocationController.js
+++ public/javascripts/LocationController.js
@@ -1,25 +1,25 @@
-(function() {
+(function () {
     var app = angular.module('lunchmonkey', []);
 
-    app.controller('LocationController', function($scope, $http){
+    app.controller('LocationController', function ($scope, $http) {
 
         $http({method: 'GET', url: '/locations'}).
-            success(function(data, status, headers, config) {
+            success(function (data, status, headers, config) {
                 console.log("success: " + data)
                 $scope.locations = data
             }).
-            error(function(data, status, headers, config) {
+            error(function (data, status, headers, config) {
                 console.log("error")
             });
 
-        $scope.randomButton = function() {
+        $scope.randomButton = function () {
             $http({method: 'GET', url: '/locations/random'}).
-                success(function(data, status, headers, config) {
+                success(function (data, status, headers, config) {
                     console.log("success: " + data)
                     $scope.random = data
                     document.getElementById("random").style.display = 'block'
                 }).
-                error(function(data, status, headers, config) {
+                error(function (data, status, headers, config) {
                     console.log("error")
                     document.getElementById("random").style.display = 'none'
                 });
