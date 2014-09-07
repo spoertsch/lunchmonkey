2fcf421e0feaf9f00cf8e9fa1691a27243da9a69
spoertsch <spoertsch@gmail.com>
2014-09-07 21:25:36 +0200

    Updated random location visualisation

--- public/javascripts/LocationController.js
+++ public/javascripts/LocationController.js
@@ -12,12 +12,20 @@
                 console.log("error")
             });
 
+        var old = null
+
         $scope.randomButton = function () {
             $http({method: 'GET', url: '/locations/random'}).
                 success(function (data, status, headers, config) {
                     console.log("success: " + data)
                     $scope.random = data
                     document.getElementById("random").style.display = 'block'
+
+                    document.getElementById(data.name).style.backgroundColor = '#dff0d8'
+                    if(old != null) {
+                        old.style.backgroundColor = '#ffffff'
+                    }
+                    old = document.getElementById(data.name)
                 }).
                 error(function (data, status, headers, config) {
                     console.log("error")
