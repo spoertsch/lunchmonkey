c6aa0db38ab59e82285174b4fed9440b609f92bc
stefantomm <david.dastard@gmx.de>
2014-09-09 00:33:16 +0200

    #8 Add links to locations

--- app/models/Location.scala
+++ app/models/Location.scala
@@ -5,7 +5,7 @@ import play.api.libs.json.Json
 /**
  * Created by jst on 04.09.14.
  */
-case class Location(name: String, foodStyle: String) {
+case class Location(name: String, foodStyle: String, url: String) {
 
 }
 
@@ -14,21 +14,25 @@ object Location {
   implicit val taskWrites = Json.writes[Location]
   implicit val taskReads = Json.reads[Location]
 
-  val defaultLocations = List(Location("MoschMosch", "asian"),
-    Location("Thai Express", "asian"),
-    Location("DaSalvo", "italian"),
-    Location("Vapiano", "italian"),
-    Location("Zum Gutenberg", "greek"),
-    Location("Red Chili", "asian"),
-    Location("Awake", "various"),
-    Location("Extrablatt", "various"),
-    Location("Proviantmagazin", "various"),
-    Location("Rewe", "food store"),
-    Location("GBC Döner", "döner"),
-    Location("Subway", "sandwich"),
-    Location("Metzger GBC", "butcher"),
-    Location("Currywurst GBC", "imbiss"),
-    Location("Imbiss", "imbiss"),
-    Location("Pizzeria Bretzenheim", "italian"))
+  val defaultLocations = List(Location("MoschMosch", "asian", "http://moschmosch.com/sites/default/files/mosch_herbst_goetheplatz_kw33-14_0.pdf"),
+    Location("Thai Express", "asian", ""),
+    Location("DaSalvo", "italian", ""),
+    Location("Vapiano", "italian", "http://de.vapiano.com/de/menue/pasta/"),
+    Location("Zum Gutenberg", "greek", ""),
+    Location("Red Chili", "asian", "http://www.redchilli-thai.de/speisen/mittagstisch"),
+    Location("Awake", "various", "https://de-de.facebook.com/cafeawake"),
+    Location("Extrablatt", "various", "http://www.cafe-extrablatt.com/cms/front_content.php?idcat=956&standort_id=60"),
+    Location("Proviantmagazin", "various", "http://www.proviant-magazin.de/?essen_und_trinken&mittagstisch"),
+    Location("Rewe", "food store", ""),
+    Location("GBC Döner", "döner", ""),
+    Location("KPC Döner", "döner", ""),
+    Location("Subway", "sandwich", ""),
+    Location("Metzger GBC", "butcher", ""),
+    Location("Currywurst GBC", "imbiss", ""),
+    Location("Imbiss", "imbiss", "http://www.fe-mainz.de/index.php?s=Imbiss"),
+    Location("Gasthof Grün", "various", "http://www.gasthofgruen.de/mittagstisch/"),
+    Location("Baron", "various", "http://www.baron-mainz.de/restaurant/speisen/mittagstisch/"),
+    Location("Eisgrub", "german", ""),
+    Location("Pizzeria Bretzenheim", "italian", ""))
 
 }
